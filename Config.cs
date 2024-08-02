58d7e483268b5c3ba5e9c2d49a84cf6f3c3687aa
louisharding <lgharding0@gmail.com>
2024-08-02 13:42:58 +0100

    first commmit

---
+++ Config.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorApp1
+{
+    public class Config
+    {
+        public string? DiscordKey { get; init; }  
+    }
+}
---
+++ DiscordTimer.cs
@@ -0,0 +1,43 @@
+﻿
+namespace BlazorApp1
+{
+    public class DiscordTimer : IHostedService, IDisposable
+    {
+        private ILogger<DiscordTimer> _logger;
+        private Timer? _timer;
+        private Config _config;
+
+
+        public DiscordTimer(ILogger<DiscordTimer> logger, Config config) 
+        {
+            _config = config;
+            _logger = logger;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DoWork(object? state)
+        {
+            _logger.LogInformation(_config.DiscordKey);
+        }
+
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Timed Hosted Service running.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+       }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
